a3712141e07aeb78636d7703b6796e35499ecc04
castdrian <adrifcastr@gmail.com>
2022-07-25 11:16:30 +0200

    initial commit

---
+++ reIMSAP/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Security.Principal;
+using System.Security.Cryptography;
+using static reIMSAP.Util;
+using System.Diagnostics;
+
+namespace reIMSAP
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            if (!IsCurrentProcessAdmin()) if (!Auth()) Application.Current.Shutdown();
+        }
+    }
+}
---
+++ reIMSAP/Util.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace reIMSAP
+{
+    internal class Util
+    {
+        private static String[] users = {"e2711bfd2da9999831dc1cbf539829692ac8a135ad58a7e4783091a609be4a31"};
+        public static String sha256_hash(string value)
+        {
+            StringBuilder Sb = new StringBuilder();
+
+            using (var hash = SHA256.Create())
+            {
+                Encoding enc = Encoding.UTF8;
+                byte[] result = hash.ComputeHash(enc.GetBytes(value));
+
+                foreach (byte b in result)
+                    Sb.Append(b.ToString("x2"));
+            }
+
+            return Sb.ToString();
+        }
+
+        public static bool Auth() 
+        {
+            if (users.Contains(sha256_hash(Environment.UserName))) return true;
+            else return false;
+        }
+
+        public static bool IsCurrentProcessAdmin()
+        {
+            using var identity = System.Security.Principal.WindowsIdentity.GetCurrent();
+            var principal = new System.Security.Principal.WindowsPrincipal(identity);
+            return principal.IsInRole(System.Security.Principal.WindowsBuiltInRole.Administrator);
+        }
+    }
+}
