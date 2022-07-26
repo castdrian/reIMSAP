1cc9772ba350910eb977a5f035af156cc417a702
castdrian <adrifcastr@gmail.com>
2022-07-26 08:40:06 +0200

    gitignore

--- reIMSAP/MainWindow.xaml.cs
+++ reIMSAP/MainWindow.xaml.cs
@@ -7,6 +7,9 @@ using System.Security.Cryptography;
 using static reIMSAP.Util;
 using static reIMSAP.SQL;
 using System.Diagnostics;
+using Npgsql;
+using System.Windows.Controls;
+using System.Data;
 
 namespace reIMSAP
 {
@@ -18,13 +21,47 @@ namespace reIMSAP
         public MainWindow()
         {
             InitializeComponent();
-            if (!IsCurrentProcessAdmin()) if (!Auth()) Application.Current.Shutdown();
+            if (!IsCurrentProcessAdmin()) if (!Auth()) {
+                    MessageBox.Show("You are not authorized to use this application.", "reIMS - Admin Panel", MessageBoxButton.OK, MessageBoxImage.Error);
+                    Application.Current.Shutdown(); 
+                }
             login.Content = $"Logged in as {Environment.UserName}";
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            Connect(host.Text);
+            ShowData(host.Text,dbgrid);
+            additem.IsEnabled = true;
+            importdb.IsEnabled = true;
+            exportdb.IsEnabled = true;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Developed by: Adrian Fernandez Castro, reh 7667, EDU\n© 2022 - All rights reserved", "reIMS - Admin Panel", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Row_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (sender != null)
+            {
+#pragma warning disable CS8600 // Converting null literal or possible null value to non-nullable type.
+                DataGridRow row = sender as DataGridRow;
+#pragma warning disable CS8602 // Dereference of a possibly null reference.
+                DataRowView SelectedRow = (DataRowView)row.Item;
+#pragma warning restore CS8602 // Dereference of a possibly null reference.
+                string item = SelectedRow["item"].ToString() ?? "";
+                string amount = SelectedRow["amount"].ToString() ?? "";
+                string connector = SelectedRow["connector"].ToString() ?? "";
+                string color = SelectedRow["color"].ToString() ?? "";
+                string type = SelectedRow["type"].ToString() ?? "";
+                string length = SelectedRow["length"].ToString() ?? "";
+                string cclass = SelectedRow["class"].ToString() ?? "";
+                string mode = SelectedRow["mode"].ToString() ?? "";
+
+                Window edit = new EditWindow();
+                edit.ShowDialog();
+            }
         }
     }
 }
--- reIMSAP/SQL.cs
+++ reIMSAP/SQL.cs
@@ -1,31 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Controls;
 using Npgsql;
 
 namespace reIMSAP
 {
     internal class SQL
     {
-        public static void Connect(String host)
+        public static void ShowData(String host, DataGrid dbgrid)
         {
             var cs = $"Host={host};Username=pi;Database=reims";
 
             using var con = new NpgsqlConnection(cs);
             con.Open();
 
-            var sql = "SELECT version()";
+            NpgsqlCommand myTableCommand = new NpgsqlCommand("select * from cords", con);
+            DataTable dt = new DataTable();
+            NpgsqlDataAdapter a = new NpgsqlDataAdapter(myTableCommand);
 
-            using var cmd = new NpgsqlCommand(sql, con);
-
-#pragma warning disable CS8602 // Dereference of a possibly null reference.
-            var version = cmd.ExecuteScalar().ToString();
-#pragma warning restore CS8602 // Dereference of a possibly null reference.
-            Debug.WriteLine($"PostgreSQL version: {version}");
+            a.Fill(dt);
+            dbgrid.ItemsSource = dt.DefaultView;
+            con.Close();
         }
     }
 }
